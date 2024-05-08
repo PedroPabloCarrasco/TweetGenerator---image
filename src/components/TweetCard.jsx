c8fe1023fb74d6eb9944f656ba4f0285ab108a44
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2024-05-08 15:40:59 -0400

    Formulario y lectura de información OK

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -3,56 +3,182 @@ import styles from '../styles/Card.module.css';
 import Verified from './icons/Verified';
 import Comments from './icons/Comments'
 import Retwit from './icons/Retwit'
-import Share from './icons/Share'
 import Likes from './icons/Likes'
+import Share from './icons/Share'
+import UserImage from './icons/UserImage'
+import Dots from './icons/Dots'
+
+import { addLinks } from '../helpers/addLinks';
+import { useRef, useState } from 'react';
+import { blobToData } from '../helpers/blobToData';
+import { getDate } from '../helpers/getDate';
+import { formatCount } from '../helpers/formatCount';
+
+import { toPng } from 'html-to-image'
+
 
 const TweetCard = () => {
+    const currentDate = new Date().toISOString().slice(0, 16);
     const paragraph = "This is a sample tweet. @mentions, #hashtags, https://links.com are all automatically converted."
-    return ( 
+
+    const [avatar, setAvatar] = useState();
+    const [name, setName] = useState("Pedro Pablo");
+    const [username, setUsername] = useState("@ppablo");
+    const [image, setImage] = useState();
+    const [verified, setVerified] = useState(true)
+    const [date, setDate] = useState("1h");
+    const [content, setContent] = useState(paragraph);
+    const [size, setSize] = useState("95")
+    const [comment, setCommet] =  useState("")
+    const [likes, setLikes] =  useState("")
+    const [retweet, setRetweet] =  useState("")
+
+    const uploadAvatar = async e => {
+        const objAvatar = e.target.files[0];
+        setAvatar(await blobToData(objAvatar))
+    }
+    const uploadImage = async e => {
+        const objAvatar = e.target.files[0];
+        setImage(await blobToData(objAvatar))
+    }
+    const handleDate = e => {
+        //console.log(e.target.value);
+        setDate(getDate(e.target.value));
+    }
+    const handleTextarea = e => {
+        setSize(content.length)
+        setContent(e.target.value)
+    } 
+    const handleComments = e => {
+        const countClean = formatCount(e.target.value);
+        setCommet(countClean);
+    }
+    const handleLikes = e => {
+        const countClean = formatCount(e.target.value);
+        setLikes(countClean);
+    }
+    const handleRetweets = e => {
+        const countClean = formatCount(e.target.value);
+        setRetweet(countClean);
+    }
+    const ref =  useRef(null);
+    const downloadImage = async e => {
+        const dataUrl = await toPng(ref.current);
+        const link = document.createElement('a')
+        link.download = "tweet-image.png"
+        link.href = dataUrl
+        link.click();
+    }
+
+    return (
+        <>
         <div className={styles.hero}>
-            <section className={styles.twitCard}>
+            <h1>Tweet Generator</h1>
+            <section className={styles.twitCard} ref={ref}>
                 <picture className={styles.userImage}>
-                    <img src="/elon.png" alt="" />
+                    { avatar ? <img src={avatar} alt="avatar" /> : <UserImage/> }
+                    
                 </picture>
                 <div className={styles.content}>
                     <header>
                         <div>
-                            <h3>Elon Musk</h3>
-                            <Verified/>
-                            <p>@elonmusk . 2h</p>
+                            <h3>{name}</h3>
+                            { verified && <Verified/> }
+                            <p>{username} • {date}</p>
                         </div>
+                        <Dots/>
                     </header>
-                    <p>{paragraph}</p>
-                    <div className={styles.images}>
-                        <img src="./elon.png" alt="" />
-
-                    </div>
+                    <p dangerouslySetInnerHTML={{__html:addLinks(content) }}></p>
+                    {
+                        image &&
+                        <div className={styles.images}>
+                            <img src={image} alt="tweet image" />
+                        </div>
+                    }
                     <footer>
                         <div>
                             <Comments/>
-                            <span>34</span>
+                            <span>{comment}</span>
                         </div>
-
-                        <div>
-                            <Likes/>
-                            <span>34</span>
-                        </div>
-
                         <div>
                             <Retwit/>
-                            <span>34</span>
+                            <span>{likes}</span>
                         </div>
-
+                        <div>
+                            <Likes/>
+                            <span>{retweet}</span>
+                        </div>
                         <div>
                             <Share/>
-                            <span>34</span>
                         </div>
                     </footer>
-
                 </div>
             </section>
         </div>
-     );
+        <div className={styles.bgWave}></div>
+        <div className={styles.formInputs}>
+            <div className={styles.container}>
+                <form >
+                    <span>
+                        <label>Avatar:</label>
+                        <input type="file" onChange={uploadAvatar} accept=".png, .jpg, .svg"/>
+                    </span>
+                    <span>
+                        <label>Name:</label>
+                        <input type="text" onChange={e=>setName(e.target.value)} value={name}/>
+                    </span>
+                    <span>
+                        <label>Username:</label>
+                        <input type="text" onChange={e=>setUsername(e.target.value)} value={username}/>
+                    </span>
+                    <span>
+                        <label>Fecha del Tweet:</label>
+                        <input type="datetime-local" onChange={handleDate} max={currentDate}/>
+                    </span>
+                    <span>
+                        <label>Imagen del tweet:</label>
+                        <input type="file" onChange={uploadImage} accept=".png, .jpg, .svg"/>
+                    </span>
+                    <span>
+                        <button type='button'
+                            onClick={()=>setVerified(current=>!current)}
+                            className={verified ? styles.btnVerified : ""}
+                        >Verificación
+                        </button>
+                    </span>
+                    <span>
+                        <label>Content: {`${size}/280`}</label>
+                        <textarea cols="30" rows="5"
+                            onChange={handleTextarea}
+                            value={content}
+                            maxLength="281"
+                        ></textarea>
+                    </span>
+                    <div>
+                        <span>
+                            <label>Comentarios:</label>
+                            <input type="number" onChange={handleComments}/>
+                        </span>
+                        <span>
+                            <label>RePost:</label>
+                            <input type="number" onChange={handleLikes}/>
+                        </span>
+                        <span>
+                            <label>Likes:</label>
+                            <input type="number" onChange={handleRetweets}/>
+                        </span>
+                    </div>
+                    <button
+                        className={styles.btnDownload}
+                        type="button"
+                        onClick={downloadImage}
+                    >Descargar Tweet</button>
+                </form>
+            </div>
+            
+        </div>
+        </>
+    );
 }
  
 export default TweetCard;
